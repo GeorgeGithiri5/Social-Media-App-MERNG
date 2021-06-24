f5fc507231225b7c03d9a700ef81c72dc02b1b53
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-06-24 18:28:40 +0300

    Client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from "./pages/Register";
+import MenuBar from './components/Menu'
+
+function App() {
+  return (
+    <Router>
+      <Container>
+      <MenuBar/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
